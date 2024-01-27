a3e840512e8eefe8da4c0d7abf6c472cc91e05e8
Kelvin César de Andrade <kelvincandrade@gmail.com>
2024-01-27 19:14:59 -0300

    Musica no html de teste do grid

--- src/grid/app.js
+++ src/grid/app.js
@@ -3,7 +3,6 @@ let toolbar = document.getElementById("toolbar");
 const GridPainter = new Painter('drawing-board', 'toolbar', 10);
 const limit = 15;
 
-
 GridPainter.setPaintColor('#000');
 GridPainter.setPaintLimit(limit);
 
@@ -18,3 +17,18 @@ toolbar.addEventListener("click", function(event) {
         GridPainter.setPaintLimit(limit);
     }
 });
+
+function game_music() {
+    myAudio.play();
+    if (myAudio.paused == true) {
+        myAudio.play();
+    }
+}
+let myAudio = new Audio();
+
+myAudio.src = '../../sound/trilha_initial.mp3';
+
+window.onload = setInterval(game_music, 1000 / 10); //10fps
+
+
+
